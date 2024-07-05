c24b8a7c1989ffc85f3d3f62231890dd3848e550
Ashish-khangagwal <ashishkhanagwal2001@gmail.com>
2024-07-05 15:09:28 +0530

    Revisiting concepts

---
+++ Learn-JavaScript/script.js
@@ -0,0 +1,13 @@
+console.log(`Hello`);
+console.log(`Ashish`);
+
+// window.alert(`I'm coding`);
+
+// This is a comment
+/*
+ This is a 
+ multiline comment
+*/
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `Ashish`;
